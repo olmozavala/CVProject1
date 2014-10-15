2aa60758593920f14da642a00e1b3e9fd980a21a
dabryan <dabryan@gmail.com>
2014-10-15 17:55:52 -0400

    Sift features and descriptors
    Comparing all the descriptors takes too long, it was still going after
    12+hours I stopped it at 47%

---
+++ sift.m
@@ -0,0 +1,51 @@
+clf
+clear
+close all
+addpath(genpath('externalLib'));
+addpath(genpath('Variables'));
+
+% Running in parallel, check if the pool of threads is already open
+if matlabpool('size') == 0 
+    infoLocal = parcluster('local');
+    maxWorkers = infoLocal.NumWorkers;
+    matlabpool('open',maxWorkers);
+end
+
+totalImages = 1000;
+
+disp('Reading all the images...');
+origImages = readImages(totalImages, 'corel');
+    
+
+%% Calculate descriptors for images
+tic;
+display('Detecting SIFT features and descriptors...');
+parfor_progress(totalImages);
+if exist('Variables/descriptors.mat', 'file')
+    load('descriptors.mat','descriptors');
+else
+    for i=1:totalImages
+        [fa, descriptors{i}] = vl_sift(single(rgb2gray(uint8(squeeze(origImages(i,:,:,:))))));
+        parfor_progress;
+    end
+    save('Variables/descriptors.mat','descriptors');
+end
+toc;
+
+
+%% Compare descriptors to determine scores 
+tic;
+display('Computing Scores...');
+parfor_progress(totalImages*totalImages);
+if exist('Variables/scores.mat', 'file')
+    load('scores.mat','scores');
+else
+    for i=1:totalImages
+        for j=1:totalImages
+            [matches, scores{i,j}] = vl_ubcmatch(descriptors{i}, descriptors{j});
+            parfor_progress;
+        end
+    end
+    save('Variables/scores.mat','scores');
+end
+toc;
