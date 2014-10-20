3957a677f0670347e812a5d6486fe44433dfb182
Olmo Zavala <olmozavala@gmail.com>
2014-10-19 20:44:52 -0400

    Matlab publish file

---
+++ PublishAll.m
@@ -0,0 +1,9 @@
+
+publish('Problem1And2.m','format','pdf','evalCode',true, 'showCode', true);
+publish('filterImages.m','format','pdf','evalCode',false, 'showCode', true);
+publish('sift.m','format','pdf','evalCode',false, 'showCode', true);
+publish('maskLoGFunc.m','format','pdf','evalCode',false, 'showCode', true);
+publish('readImages.m','format','pdf','evalCode',false, 'showCode', true);
+publish('computeHistDist.m','format','pdf','evalCode',false, 'showCode', true);
+publish('computeHist.m','format','pdf','evalCode',false, 'showCode', true);
+
